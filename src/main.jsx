cff2d266679f352dcbcbdefdce2cba34e14dd7f4
Sunny Saxena <sunnysaxena19@gmail.com>
2023-07-28 18:08:42 +0530

    all

--- src/main.jsx
+++ src/main.jsx
@@ -4,7 +4,7 @@ import App from "./App";
 import "./styles/app.scss";
 import { createContext } from "react";
 
-export const server = "https://nodejs-todoapp-i30p.onrender.com";
+export const server = "https://nodejs-todoapp-i30p.onrender.com/api/v1";
 
 export const Context = createContext({ isAuthenticated: false });
 
